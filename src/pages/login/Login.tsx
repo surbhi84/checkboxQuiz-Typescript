6c9ef3f3589c1ea2170d874cde95b9955b9e9fa0
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-05-16 03:22:09 +0530

    add:reducer and usestate taking input values

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -5,18 +5,28 @@ import "./login.css";
 
 export const Login = () => {
   const [isPwdVisible, setIsPwdVisible] = useState(false);
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPwd, setLoginPwd] = useState("");
 
   return (
     <div className="flex-center mv-xl">
       <div className="flex-center flex-col mg-m">
         <label className="mg-xs flex-col ">
           Email
-          <input type="text" className="w15" />
+          <input
+            type="text"
+            className="w15"
+            onChange={(e) => e.target.value.test([\a-zA-Z\]) ? setLoginEmail(e.target.value) :}
+          />
         </label>
         <label className="mg-xs flex-col">
           Password
           <div className="flex-row input-eye-holder">
-            <input type={isPwdVisible ? "text" : "password"} className="w15" />
+            <input
+              type={isPwdVisible ? "text" : "password"}
+              className="w15"
+              onChange={(e) => setLoginPwd(e.target.value)}
+            />
             {isPwdVisible ? (
               <FaEyeSlash
                 onClick={() => setIsPwdVisible((p) => !p)}
@@ -30,7 +40,12 @@ export const Login = () => {
             )}
           </div>
         </label>
-        <button className="auth-btn border-prim mg-xs">Login</button>
+        <button
+          className="auth-btn border-prim mg-xs"
+          onClick={() => console.log(loginEmail, loginPwd)}
+        >
+          Login
+        </button>
         <div className="small-text">or</div>
         <Link to="/signup" className="text-dec-none prim-text">
           Signup
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,42 +1,149 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 export const Signup = () => {
   const [isPwdVisible, setIsPwdVisible] = useState(false);
+  const [passwd, setPasswd] = useState("");
+
+  function passwordHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value === passwd) {
+      signupDispatch({ type: "PASSWORD", payload: e.target.value });
+    }
+    console.error("passwords don't match");
+  }
+
+  const initalSignupDetails = {
+    firstName: "",
+    lastName: "",
+    userName: "",
+    dob: "",
+    email: "",
+    contact: 384734387,
+    password: "",
+  };
+
+  interface SignupObject {
+    firstName: string;
+    lastName: string;
+    userName: string;
+    dob: string;
+    email: string;
+    contact: number;
+    password: string;
+  }
+
+  type SignupReducerActions =
+    | {
+        type:
+          | "FIRSTNAME"
+          | "LASTNAME"
+          | "USERNAME"
+          | "DOB"
+          | "EMAIL"
+          | "PASSWORD";
+        payload: string;
+      }
+    | { type: "CONTACT"; payload: number };
+
+  const signupReducer = (
+    state: SignupObject,
+    action: SignupReducerActions
+  ): SignupObject => {
+    switch (action.type) {
+      case "FIRSTNAME":
+        return { ...state, firstName: action.payload };
+      case "LASTNAME":
+        return { ...state, lastName: action.payload };
+      case "USERNAME":
+        return { ...state, userName: action.payload };
+      case "DOB":
+        return { ...state, dob: action.payload };
+      case "EMAIL":
+        return { ...state, email: action.payload };
+      case "CONTACT":
+        return { ...state, contact: action.payload };
+      case "PASSWORD":
+        return { ...state, password: action.payload };
+    }
+  };
+
+  const [signupDetails, signupDispatch] = useReducer(
+    signupReducer,
+    initalSignupDetails
+  );
 
   return (
     <div className="flex-center mv-xl">
-      <div className="flex-center flex-col mg-m ">
+      <div className="flex-center flex-col mg-m">
         <div className="flex-row">
           <label className="mg-xs flex-col">
             First Name
-            <input type="text" className="w15" />
+            <input
+              type="text"
+              className="w15"
+              onChange={(e) =>
+                signupDispatch({ type: "FIRSTNAME", payload: e.target.value })
+              }
+            />
           </label>
           <label className="mg-xs flex-col">
             Last Name
-            <input type="text" className="w15" />
+            <input
+              type="text"
+              className="w15"
+              onChange={(e) =>
+                signupDispatch({ type: "LASTNAME", payload: e.target.value })
+              }
+            />
           </label>
         </div>
         <div className="flex-row">
           <label className="mg-xs flex-col">
             Username
-            <input type="text" className="w15" />
+            <input
+              type="text"
+              className="w15"
+              onChange={(e) =>
+                signupDispatch({ type: "USERNAME", payload: e.target.value })
+              }
+            />
           </label>
           <label className="mg-xs flex-col">
             Dob
-            <input type="text" className="w15" />
+            <input
+              type="date"
+              className="w15"
+              onChange={(e) =>
+                signupDispatch({ type: "DOB", payload: e.target.value })
+              }
+            />
           </label>
         </div>
 
         <div className="flex-row">
           <label className="mg-xs flex-col">
             Email
-            <input type="text" className="w15" />
+            <input
+              type="text"
+              className="w15"
+              onChange={(e) =>
+                signupDispatch({ type: "EMAIL", payload: e.target.value })
+              }
+            />
           </label>
           <label className="mg-xs flex-col">
             Contact
-            <input type="text" className="w15" />
+            <input
+              type="number"
+              className="w15"
+              onChange={(e) =>
+                signupDispatch({
+                  type: "CONTACT",
+                  payload: Number(e.target.value),
+                })
+              }
+            />
           </label>
         </div>
         <div className="flex-row">
@@ -46,6 +153,7 @@ export const Signup = () => {
               <input
                 type={isPwdVisible ? "text" : "password"}
                 className="w15"
+                onChange={(e) => setPasswd(e.target.value)}
               />
               {isPwdVisible ? (
                 <FaEyeSlash
@@ -66,6 +174,7 @@ export const Signup = () => {
               <input
                 type={isPwdVisible ? "text" : "password"}
                 className="w15"
+                onChange={(e) => passwordHandler(e)}
               />
               {isPwdVisible ? (
                 <FaEyeSlash
@@ -81,7 +190,12 @@ export const Signup = () => {
             </div>
           </label>
         </div>
-        <button className="auth-btn border-prim mg-s">Signup</button>
+        <button
+          className="auth-btn border-prim mg-s"
+          onClick={() => console.log(signupDetails)}
+        >
+          Signup
+        </button>
         <div className="small-text">or</div>
         <Link to="/login" className="text-dec-none prim-text">
           Already have an account
